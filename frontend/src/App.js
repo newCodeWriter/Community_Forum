b2695a869e3d6405cd87c7fb54e564eb5cf3dc59
Shay Brown <fire328@ymail.com>
2020-10-20 19:55:25 -0400

    setup and connect express and react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Subject from './Subject';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Subject />
+    </div>
+  );
+}
+
+export default App;
