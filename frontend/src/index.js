bdda217923aded0e9126a9b99b33e53e71fc0a6a
Shay Brown <fire328@ymail.com>
2020-10-24 03:19:02 -0400

    remove commented code; edit routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import './index.css';
 // import App from './App';
 import Home from './components/home';
 import Error from './components/error';
+import Test from './components/test';
 
 const Routeit = () => {
   return (
     <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={() => <div>About this</div>}/>
+          <Route exact path="/" render={() => <Redirect to="/test"/>}/>
           <Route path="/home" component={Home}/>
+          <Route path='/test' component={Test}/>
           <Route component={Error} />
         </Switch>
     </BrowserRouter>
