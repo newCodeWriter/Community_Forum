5eb506a4e06cdbdf1250667722ec41bef445713d
Shay Brown <fire328@ymail.com>
2020-11-01 14:13:07 -0500

    add router link;

--- frontend/src/components/subject.js
+++ frontend/src/components/subject.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { math } from '../constants'
 import Button from 'react-bootstrap/Button'
-// import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchCategoryInfo } from '../actions'
-// import { userAuth } from '../checkAuth'
 
 
 
@@ -28,28 +27,26 @@ class Subject extends Component {
         }
     }
 
-    handleClick = () => {
-        this.props.history.push(`${this.props.match.url}/post`)
-    }
-
     render(){
         return (
             <div className="mt-5 w-75 mx-auto">
                 <h2>{this.state.subject.name}</h2>
                 <Button variant="primary" className="mb-4 pl-3 pr-3 pt-2 pb-2" onClick={()=>{this.props.history.push(`${this.props.match.url}/question`)}}>New Question?</Button>
                 {this.state.subjectInfo.map((subject, key) => (
-                    <div key={key} onClick={this.handleClick} className="border border-light bg-light d-block mb-3 p-3 subject-container">
-                        <div className="row">
-                            <div className="col-md-4 text-muted small">{subject.date}</div>
-                            <div className="col-md-8 text-success text-right small">by: {subject.username}</div>
+                    <Link key={key} to={`${this.props.match.url}/${subject.question_id}`}>
+                        <div id={subject.question_id} className="border border-light bg-light d-block mb-3 p-3 subject-container">
+                            <div className="row">
+                                <div className="col-md-4 text-muted small">{subject.date}</div>
+                                <div className="col-md-8 text-success text-right small">by: {subject.username}</div>
+                            </div>
+                            <div className="row mt-3 w-100">
+                                <div className="col">{subject.new_question}</div>
+                            </div>
+                            <div className="row mt-2 text-muted" >
+                                <div className="col text-right small">Answers: {subject.answers}</div>
+                            </div>
                         </div>
-                        <div className="row mt-3 w-100">
-                            <div className="col">{subject.new_question}</div>
-                        </div>
-                        <div className="row mt-2 text-muted">
-                            <div className="col text-right small">Answers: {subject.answers}</div>
-                        </div>
-                    </div>
+                    </Link>
                 ))}
             </div> 
         )
