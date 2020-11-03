bdebeaf3b937e026b2e0539128aeadeed425c784
Shay Brown <fire328@ymail.com>
2020-11-03 02:27:55 -0500

    add dev tools extension;

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,25 @@ import { createLogger } from 'redux-logger'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import mathApp from './reducers/reducers'
 import './index.css';
 
 const loggerMiddleware = createLogger()
 
-const store = createStore(mathApp, applyMiddleware(
+const store = createStore(mathApp, composeWithDevTools(
+  applyMiddleware(
     thunkMiddleware, // lets us dispatch() functions
     loggerMiddleware // neat middleware that logs actions
   )
-)
+));
+
+// const store = createStore(mathApp, applyMiddleware(
+//     thunkMiddleware, // lets us dispatch() functions
+//     loggerMiddleware // neat middleware that logs actions
+//   )
+// )
 
 ReactDOM.render(
   <Provider store={store}>
