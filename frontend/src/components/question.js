96ba3b8537d29297f82578389683ee1408a2e7b4
Shay Brown <fire328@ymail.com>
2021-02-22 01:32:50 -0500

    clean up formatting; switch to class;

--- frontend/src/components/question.js
+++ frontend/src/components/question.js
@@ -1,74 +1,98 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button'
-import axios from 'axios'
-import { connect } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import { copyState } from '../localStorage'
-import { fetchCategoryInfo } from '../actions'
+/** @format */
 
-function Question({ match, dispatch }){
-    const [ error, setError ] = useState({
-        mark: '',
-        length: ''
-    })
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { copyState } from '../localStorage';
+import { fetchCategoryInfo } from '../actions';
 
-    const history = useHistory();
+class Question extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: '',
+			question: '',
+		};
+	}
 
-    const { userName, userId } = copyState().authentication
+	handleForm = (event) => {
+		event.preventDefault();
+		const text = this.state.question;
+		const data = {
+			user: copyState().authentication.userId,
+			category: this.props.match.params.subjectId,
+			question: text,
+		};
+		if (text.endsWith('?') && text.length >= 10) {
+			axios
+				.post('/question', data)
+				.then(this.props.dispatch(fetchCategoryInfo(this.props.match.params.subjectId)))
+				.then(this.props.history.go(-1))
+				.catch(console.error());
+		} else {
+			this.setState({
+				error:
+					'Your question must end with a "?" and be at least 10 characters long.',
+			});
+		}
+	};
 
-    function handleForm(event){
-        event.preventDefault();
-        let text = document.getElementById('question').value; 
-        let user_id = userId;
-        let data = {
-            user: user_id, 
-            category: match.params.subjectId, 
-            question: text
-        }
-        if(text.endsWith('?') && text.length >= 10){
-            axios.post('/question', data)
-            .then(history.goBack())
-            .then(dispatch(fetchCategoryInfo(match.params.subjectId)))
-            .catch(console.error())
-        }
-        else if(!text.endsWith('?') && text.length < 10){
-            setError({mark: 'Your question does not end with a "?"', 
-                    length: 'Your question must be at least 10 characters long.'})
-        }
-        else if(!text.endsWith('?')){
-            setError({mark: 'Your question does not end with a "?"', 
-                    length: ''})
-        }
-        else{
-            setError({mark: '', 
-            length: 'Your question must be at least 10 characters long.'}) 
-        }
+	handleReturn = () => this.props.history.go(-1)
 
-    }
 
-    function handleReturn(){
-        history.goBack()
-    }
-    function handleTextChange(){
-        setError({mark: '', 
-                length: ''})
-    }
+	handleTextChange = (event) => {
+		this.setState({
+			error: '',
+			question: event.target.value,
+		});
+	};
 
-    return (
-        <div className="set-width mx-auto">
-            <form id="question_form">
-                <div className="row mt-3 text-muted">
-                    <div className="col">
-                        <div className="text-danger small">{error.mark}</div>
-                        <div className="mb-2 text-danger small">{error.length}</div>
-                        <label htmlFor="question" className="d-block">{userName.toUpperCase()}, enter your question:</label>
-                        <textarea id="question" name="question" className="p-3 mb-4 w-100" onChange={handleTextChange} rows="7" cols="56" required autoFocus></textarea>
-                    </div>
-                </div>
-            </form>
-            <Button form="question_form" variant="primary" type="submit" onClick={handleForm} className="p-2 mr-3">Submit</Button>
-            <Button variant="secondary" type="button" onClick={handleReturn} className="p-2">Back to Questions</Button>
-        </div>
-    )
+	render() {
+		const { userName } = copyState().authentication;
+		return (
+			<div className='set-width mx-auto'>
+				<form id='question_form'>
+					<div className='row mt-3 text-muted'>
+						<div className='col'>
+							<div className='mb-2 text-danger small'>{this.state.error}</div>
+							<label htmlFor='question' className='d-block'>
+								{userName.toUpperCase()}, enter your question:
+							</label>
+							<textarea
+								id='question'
+								name='question'
+								className='p-3 mb-4 w-100'
+								value={this.state.question}
+								onChange={this.handleTextChange}
+								rows='7'
+								cols='56'
+								required
+								autoFocus
+							></textarea>
+						</div>
+					</div>
+				</form>
+				<Button
+					form='question_form'
+					variant='primary'
+					type='submit'
+					onClick={this.handleForm}
+					className='p-2 mr-3'
+				>
+					Submit
+				</Button>
+				<Button
+					variant='secondary'
+					type='button'
+					onClick={this.handleReturn}
+					className='p-2'
+				>
+					Back to Questions
+				</Button>
+			</div>
+		);
+	}
 }
-export default connect()(Question)
+
+export default connect()(Question);
--- frontend/src/reducers/reducers.js
+++ frontend/src/reducers/reducers.js
@@ -1,70 +1,76 @@
-import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE, LOGOUT_USER, FETCH_DATA, UPDATE_USER } from '../constants';
-import { combineReducers } from 'redux'
-import decode from 'jwt-decode'
+/** @format */
+
+import {
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAILURE,
+	LOGOUT_USER,
+	FETCH_DATA,
+	UPDATE_USER,
+} from '../constants';
+import { combineReducers } from 'redux';
+import decode from 'jwt-decode';
 
 const userInfo = {
-    isAuthenticated: false, 
-    token: null,
-    userName: null,
-    userId: null, 
-    loggedIn: false, 
-    statusText: null,
-    loginAttempts: 0
-}
+	isAuthenticated: false,
+	token: null,
+	userName: null,
+	userId: null,
+	loggedIn: false,
+	statusText: null,
+	loginAttempts: 0,
+};
 
 const authentication = (state = userInfo, action) => {
-    switch (action.type) {
-        case LOGIN_USER_SUCCESS:
-            return {...state,
-                isAuthenticated: true, 
-                token: action.payload.token,
-                userName: decode(action.payload.token).name,
-                userId: decode(action.payload.token).id,
-                loggedIn: true,
-                statusText: 'You have been successfully logged in.'
-            }
-        case LOGIN_USER_FAILURE:
-            return {...state,
-                isAuthenticated: false, 
-                token: null,
-                userName: null,
-                loggedIn: false,
-                statusText: action.payload.status === 400
-                            ? 'This user does not exist.' 
-                            : 'You have entered an incorrect password',
-                loginAttempts: action.payload.attempts,
-                status: action.payload.status
-            }
-        case LOGOUT_USER:
-            return {...state,
-                isAuthenticated: false, 
-                token: null,
-                userName: null,
-                loggedIn: false,
-                statusText: 'You have been successfully logged out.'
-            }
-        case UPDATE_USER:
-            return {...state,
-                userName: action.payload
-            }
-        default:
-            return state
-    }
-}
+	switch (action.type) {
+		case LOGIN_USER_SUCCESS:
+			return {
+				...state,
+				isAuthenticated: true,
+				token: action.payload.token,
+				userName: decode(action.payload.token).name,
+				userId: decode(action.payload.token).id,
+				loggedIn: true,
+				statusText: 'You have been successfully logged in.',
+			};
+		case LOGIN_USER_FAILURE:
+			return {
+				...state,
+				isAuthenticated: false,
+				token: null,
+				userName: null,
+				loggedIn: false,
+				statusText:
+					action.payload.status === 400
+						? 'This user does not exist.'
+						: 'You have entered an incorrect password',
+				loginAttempts: action.payload.attempts,
+				status: action.payload.status,
+			};
+		case LOGOUT_USER:
+			return {
+				...state,
+				isAuthenticated: false,
+				token: null,
+				userName: null,
+				loggedIn: false,
+				statusText: 'You have been successfully logged out.',
+			};
+		case UPDATE_USER:
+			return { ...state, userName: action.payload };
+		default:
+			return state;
+	}
+};
 
 const data_request = (state = [], action) => {
-    switch (action.type) {
-        case FETCH_DATA:
-            return [
-                ...state,
-                action.payload
-            ].slice(-1)
-        default:
-            return state
-        
-    }
-}
+	switch (action.type) {
+		case FETCH_DATA:
+			return [...state, action.payload].slice(-1);
+		default:
+			return state;
+	}
+};
 
-const mathApp = combineReducers({authentication, data_request});
+const mathApp = combineReducers({ authentication, data_request });
 
-export default mathApp; 
+export default mathApp;
