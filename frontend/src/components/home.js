42ccd197b12a29260d82637bb5935f82e73a8973
Shay Brown <fire328@ymail.com>
2021-02-22 01:29:51 -0500

    clean up code;

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,70 +1,107 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { Route, Switch, NavLink } from "react-router-dom"
-import Dropdown from 'react-bootstrap/Dropdown'
-import NavItem from 'react-bootstrap/NavItem'
-import Subject from './subject'
-import Question from './question'
-import Post from './post'
-import ChangeAcct from './changeAcct'
-import { math } from '../constants'
-import { connect } from 'react-redux'
-import { fetchCategoryInfo, logout } from '../actions'
-import { copyState } from '../localStorage'
+/** @format */
 
-function Home({ match, dispatch }){
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import NavItem from 'react-bootstrap/NavItem';
+import Subject from './subject';
+import Question from './question';
+import Post from './post';
+import ChangeAcct from './changeAcct';
+import { math } from '../constants';
+import { connect } from 'react-redux';
+import { fetchCategoryInfo, logout } from '../actions';
+import { copyState } from '../localStorage';
 
-    const { userName } = copyState().authentication
+const Home = ({ match, dispatch }) => {
+	const { userName } = copyState().authentication;
+	const handleLogout = () => dispatch(logout());
 
-    function handleLogout(){
-        dispatch(logout());
-    }
-    
-    return(
-        <div>
-            <Navbar bg="dark" variant="dark" className="pt-2 pb-2" sticky="top">
-                <Nav className="pl-2">
-                    <Navbar.Brand href={`${match.url}`} id="math">MathQue</Navbar.Brand>
-                </Nav>
-                <Nav className="ml-auto">
-                    <Dropdown as={NavItem} className="mr-3">
-                        <Dropdown.Toggle id="nav-dropdown"><i className="fas fa-user-alt"></i> Welcome, {userName.toUpperCase()}</Dropdown.Toggle>
-                        <Dropdown.Menu className="mt-2">
-                            <Dropdown.Item href={`${match.url}/account`} className="profile">Account</Dropdown.Item>
-                            {/* add logout to dropdown for mobile devices */}
-                            <Dropdown.Item onClick={handleLogout} className="d-block d-sm-none profile">Logout</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Button variant="primary" onClick={handleLogout} className="d-none d-sm-block">Logout</Button>
-                </Nav>
-            </Navbar>
-            <div className="row w-100 m-0">
-                <div className="col-sm-5 col-md-4 col-lg-3 pl-0 pr-0">
-                    <Nav className="flex-column flex-c">
-                        {math.map(({ name, id }) => (
-                            <NavLink key={id} to={`${match.url}/${id}`}
-                                activeClassName="active-link" 
-                                className="pt-4 pb-4 pl-4 pr-3 border-bottom category-link" 
-                                onClick={() => dispatch(fetchCategoryInfo(`${id}`))}>
-                                {name} 
-                            </NavLink>
-                        ))}
-                    </Nav>
-                </div>
-                <div className="col-sm-7 col-md-8 col-lg-9 mt-5">
-                    <Switch>
-                        <Route exact path={`${match.path}`} render={() => <h3 className="ml-5">Select a category to view its questions...</h3>} />
-                        <Route path={`${match.path}/:subjectId/question`} component={Question} />
-                        <Route path={`${match.path}/:subjectId/:questionId`} component={Post} />
-                        <Route exact path={`${match.path}/account`} component={ChangeAcct} />
-                        <Route path={`${match.path}/:subjectId`} component={Subject} />
-                    </Switch>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<>
+			<Navbar bg='dark' variant='dark' className='pt-2 pb-2' sticky='top'>
+				<Nav className='pl-2'>
+					<Navbar.Brand href={`${match.url}`} id='math'>
+						MathQue
+					</Navbar.Brand>
+				</Nav>
+				<Nav className='ml-auto'>
+					<Dropdown as={NavItem} className='mr-3'>
+						<Dropdown.Toggle id='nav-dropdown'>
+							<i className='fas fa-user-alt'></i> Welcome,{' '}
+							{userName.toUpperCase()}
+						</Dropdown.Toggle>
+						<Dropdown.Menu className='mt-2'>
+							<Dropdown.Item href={`${match.url}/account`} className='profile'>
+								Account
+							</Dropdown.Item>
+							{/* add logout to dropdown for mobile devices */}
+							<Dropdown.Item
+								onClick={handleLogout}
+								className='d-block d-sm-none profile'
+							>
+								Logout
+							</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+					<Button
+						variant='primary'
+						onClick={handleLogout}
+						className='d-none d-sm-block'
+					>
+						Logout
+					</Button>
+				</Nav>
+			</Navbar>
+			<div className='row w-100 m-0'>
+				<div className='col-sm-5 col-md-4 col-lg-3 pl-0 pr-0'>
+					<Nav className='flex-column flex-c'>
+						{math.map(({ name, id }) => (
+							<NavLink
+								key={id}
+								to={`${match.url}/${id}`}
+								activeClassName='active-link'
+								className='pt-4 pb-4 pl-4 pr-3 border-bottom category-link'
+								onClick={() => dispatch(fetchCategoryInfo(id))}
+							>
+								{name}
+							</NavLink>
+						))}
+					</Nav>
+				</div>
+				<div className='col-sm-7 col-md-8 col-lg-9 mt-5'>
+					<Switch>
+						<Route
+							exact
+							path={`${match.path}`}
+							render={() => (
+								<h3 className='ml-5'>
+									Select a category to view its questions...
+								</h3>
+							)}
+						/>
+						<Route
+							path={`${match.path}/:subjectId/question`}
+							component={Question}
+						/>
+						<Route
+							path={`${match.path}/:subjectId/:questionId`}
+							component={Post}
+						/>
+						<Route
+							exact
+							path={`${match.path}/account`}
+							component={ChangeAcct}
+						/>
+						<Route path={`${match.path}/:subjectId`} component={Subject} />
+					</Switch>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default connect()(Home);
--- frontend/src/components/subject.js
+++ frontend/src/components/subject.js
@@ -1,61 +1,76 @@
-import React, { Component } from 'react'
-import Button from 'react-bootstrap/Button'
-import { Link, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { fetchCategoryInfo } from '../actions'
+/** @format */
 
-class Subject extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            questions: [],
-            redirect: false
-        }
-    }
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-    componentDidMount(){
-        this.props.dispatch(fetchCategoryInfo(this.props.match.params.subjectId));
-    }
+class Subject extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			questions: []
+		};
+	}
 
-    componentDidUpdate(prevProps){
-        if(this.props.data !== prevProps.data){
-            this.setState({questions: this.props.data})
-        }
-    }
-    
-    render(){
-        if(this.state.redirect === true){
-            return <Redirect to={`${this.props.match.url}/question`}/>
-        }
-        else{
-            return (
-                <div className="set-width mx-auto">
-                    <Button variant="primary" className="mb-4 pl-3 pr-3 pt-2 pb-2" onClick={()=> this.setState({redirect: true})}>New Question?</Button>
-                    { this.state.questions.length === 0 
-                    ? null
-                    : this.state.questions[0].map( q => (
-                        <Link key={`question_${q.question_id}`} to={`${this.props.match.url}/${q.question_id}`}>
-                            <div id={q.question_id} className="border border-light bg-light d-block mb-3 p-3 subject-container shadow">
-                                <div className="row">
-                                    <div className="col-md-5 text-muted small">{q.date}</div>
-                                    <div className="col-md-7 text-success text-right small q-user">by: {q.username}</div>
-                                </div>
-                                <div className="row mt-3 w-100">
-                                    <div className="col">{q.new_question}</div>
-                                </div>
-                                <div className="row mt-2 text-muted" >
-                                    <div className="col text-right small">Answers: {q.answers}</div>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div> 
-          )
-        }
-    }
-}; 
+	componentDidMount() {
+		axios
+			.get(`/category/${this.props.match.params.subjectId}`)
+			.then((res) => this.setState({ questions: res.data }));
+	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.data !== prevProps.data) {
+			this.setState({ questions: this.props.data[0] });
+		}
+	}
+
+    newQuestion = () => this.props.history.push(`${this.props.match.url}/question`) 
+
+	render() {
+		const { questions } = this.state;
+		const { url } = this.props.match;
+
+		return (
+			<div className='set-width mx-auto'>
+				<Button
+					variant='primary'
+					className='mb-4 pl-3 pr-3 pt-2 pb-2'
+					onClick={this.newQuestion}
+				>
+					New Question?
+				</Button>
+				{questions.map((q) => (
+					<Link
+						key={`question_${q.question_id}`}
+						to={`${url}/${q.question_id}`}
+					>
+						<div
+							id={q.question_id}
+							className='border border-light bg-light d-block mb-3 p-3 subject-container shadow'
+						>
+							<div className='row'>
+								<div className='col-md-5 text-muted small'>{q.date}</div>
+								<div className='col-md-7 text-success text-right small q-user'>
+									by: {q.username}
+								</div>
+							</div>
+							<div className='row mt-3 w-100'>
+								<div className='col'>{q.new_question}</div>
+							</div>
+							<div className='row mt-2 text-muted'>
+								<div className='col text-right small'>Answers: {q.answers}</div>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
+		);
+	}
+}
 const mapStateToProps = (state) => ({
-    data: state.data_request
-})
+	data: state.data_request,
+});
 
 export default connect(mapStateToProps)(Subject);
