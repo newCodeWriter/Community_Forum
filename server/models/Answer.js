df091268bb3fded025e669a5f2f9419cde867992
Marshay Brown <65259996+newCodeWriter@users.noreply.github.com>
2021-04-17 21:56:57 -0400

    remove category;

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -5,11 +5,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const answerSchema = new Schema({
-	category: { type: String, required: true, trim: true, lowercase: true },
-	questionId: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+	question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
 	answer: { type: String, required: true, trim: true },
 	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	date: { type: Date, required: true },
+	created: { type: String, required: true },
 });
 
 const Answer = mongoose.model("Answer", answerSchema);
