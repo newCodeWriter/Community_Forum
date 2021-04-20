02df7200c622551de3717fa3e0b395050149bbff
Marshay Brown <65259996+newCodeWriter@users.noreply.github.com>
2021-04-20 16:37:22 -0400

    convert question to functional component; add routing hooks;

--- client/src/pages/question.js
+++ client/src/pages/question.js
@@ -1,94 +1,99 @@
 /** @format */
 
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { useParams, useHistory } from "react-router-dom";
+import { useStateContext } from "../context/context";
+import axios from "axios";
 
-class Question extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: '',
-			question: '',
-		};
-	}
+const Question = () => {
+	const [error, setError] = useState(false);
+	const [question, setQuestion] = useState("");
+	const { user } = useStateContext();
+	const { subjectId } = useParams();
+	const history = useHistory();
 
-	handleForm = (event) => {
+	const goBack = () => history.go(-1);
+
+	const handleForm = async (event) => {
 		event.preventDefault();
-		const text = this.state.question;
 		const data = {
-			user: "shay",
-			category: this.props.match.params.subjectId,
-			question: text,
+			user: user.id,
+			category: subjectId,
+			question: question,
 		};
-		if (text.endsWith('?') && text.length >= 10) {
-			axios
-				.post('/question', data)
-				// .then(this.props.dispatch(fetchCategoryInfo(this.props.match.params.subjectId)))
-				.then(this.props.history.go(-1))
-				.catch(console.error());
+		if (question.endsWith("?") && question.length >= 10) {
+			try {
+				const response = await axios.post(`/api/questions/question`, data);
+				if (response.status === 200) {
+					goBack();
+				}
+			} catch (err) {
+				if (err.response) {
+					setError(true);
+				} else {
+					console.error(err.message);
+				}
+			}
 		} else {
-			this.setState({
-				error:
-					'Your question must end with a "?" and be at least 10 characters long.',
-			});
+			setError(true);
 		}
 	};
 
-	handleReturn = () => this.props.history.go(-1)
-
-
-	handleTextChange = (event) => {
-		this.setState({
-			error: '',
-			question: event.target.value,
-		});
+	const handleTextChange = (event) => {
+		setQuestion(event.target.value);
+		setError(false);
 	};
 
-	render() {
-		return (
-			<div className='set-width mx-auto'>
-				<form id='question_form'>
-					<div className='row mt-3 text-muted'>
-						<div className='col'>
-							<div className='mb-2 text-danger small'>{this.state.error}</div>
-							<label htmlFor='question' className='d-block'>
-								{'Shay'}, enter your question:
+	return (
+		<div className="set-width mx-auto">
+			<form id="question_form">
+				<div className="row mt-3 text-muted">
+					<div className="col">
+						{error && (
+							<div className="mb-2 text-danger small">
+								Your question must end with a "?" and be at least 10 characters
+								long.
+							</div>
+						)}
+						{user.name && (
+							<label htmlFor="question" className="d-block">
+								{`${user.name.toUpperCase()}, enter your question: `}
 							</label>
-							<textarea
-								id='question'
-								name='question'
-								className='p-3 mb-4 w-100'
-								value={this.state.question}
-								onChange={this.handleTextChange}
-								rows='7'
-								cols='56'
-								required
-								autoFocus
-							></textarea>
-						</div>
+						)}
+						<textarea
+							id="question"
+							name="question"
+							className="p-3 mb-4 w-100"
+							value={question}
+							onChange={handleTextChange}
+							rows="7"
+							cols="56"
+							required
+							autoFocus
+						></textarea>
 					</div>
-				</form>
-				<Button
-					form='question_form'
-					variant='primary'
-					type='submit'
-					onClick={this.handleForm}
-					className='p-2 mr-3'
-				>
-					Submit
-				</Button>
-				<Button
-					variant='secondary'
-					type='button'
-					onClick={this.handleReturn}
-					className='p-2'
-				>
-					Back to Questions
-				</Button>
-			</div>
-		);
-	}
-}
+				</div>
+			</form>
+			<Button
+				form="question_form"
+				variant="primary"
+				type="submit"
+				onClick={handleForm}
+				className="p-2 mr-3"
+			>
+				Submit
+			</Button>
+			<Button
+				variant="secondary"
+				type="button"
+				onClick={goBack}
+				className="p-2"
+			>
+				Back to Questions
+			</Button>
+		</div>
+	);
+};
 
 export default Question;
--- client/src/pages/subject.js
+++ client/src/pages/subject.js
@@ -2,26 +2,28 @@
 
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useRouteMatch, useParams } from "react-router-dom";
 import axios from "axios";
 
-const Subject = ({ match }) => {
+const Subject = () => {
 	const [questions, setQuestions] = useState([]);
 	const history = useHistory();
+	const { subjectId } = useParams();
+	const { url } = useRouteMatch();
+
+	const fetchQuestions = async () => {
+		try {
+			const response = await axios.get(`/api/questions/category/${subjectId}`);
+			setQuestions([...response.data]);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	const newQuestion = () => history.push(`${url}/question`);
 
 	useEffect(() => {
-		const fetchQuestions = async () => {
-			try {
-				const response = await axios.get(`/category/${match.params.subjectId}`);
-				setQuestions([...response.data]);
-			} catch (err) {
-				console.error(err);
-			}
-		};
 		fetchQuestions();
-	}, [match.params.subjectId]);
-
-	const newQuestion = () => history.push(`${match.url}/question`);
+	}, [subjectId]);
 
 	return (
 		<div className="set-width mx-auto">
@@ -33,15 +35,18 @@ const Subject = ({ match }) => {
 				New Question?
 			</Button>
 			{questions.map((q) => (
-				<Link key={`question_${q.question_id}`} to={`${match.url}/${q.question_id}`}>
+				<Link
+					key={`question_${q._id}`}
+					to={`${url}/${q._id}`}
+				>
 					<div
-						id={q.question_id}
+						id={q._id}
 						className="border border-light bg-light d-block mb-3 p-3 subject-container shadow"
 					>
 						<div className="row">
-							<div className="col-md-5 text-muted small">{q.question_date}</div>
+							<div className="col-md-5 text-muted small">{q.created}</div>
 							<div className="col-md-7 text-success text-right small q-user">
-								by: {q.submit_user}
+								by: {q.user.username}
 							</div>
 						</div>
 						<div className="row mt-3 w-100">
@@ -49,7 +54,7 @@ const Subject = ({ match }) => {
 						</div>
 						<div className="row mt-2 text-muted">
 							<div className="col text-right small">
-								Answers: {q.responses.length}
+								Answers: {q.answers.length}
 							</div>
 						</div>
 					</div>
